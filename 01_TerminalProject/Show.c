6fd3d9ecf66a8950c418a05428c0a6ae7a8754b7
Valery Zabrovsky <valthebrewer@yandex.ru>
2025-09-21 15:18:18 +0300

    01_TerminalProject: Implement basic file viewer

---
+++ 01_TerminalProject/Show.c
@@ -0,0 +1,65 @@
+#include <locale.h>
+#include <ncurses.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int
+wprintw_f(WINDOW *restrict win, FILE *restrict stream)
+{
+    int rc = ERR;
+    char *line = NULL;
+    size_t sz;
+
+    if (getline(&line, &sz, stream) > 0) {
+        rc = wprintw(win, line);
+    }
+
+    free(line);
+    return rc;
+}
+
+int
+main(int argc, char *argv[])
+{
+    WINDOW *frame = NULL;
+    WINDOW *view = NULL;
+    FILE *file = NULL;
+
+    file = fopen(argv[1], "r");
+
+    setlocale(LC_ALL, "");
+    initscr();
+    cbreak();
+    noecho();
+    refresh();
+
+    frame = newwin(LINES, COLS, 0, 0);
+    box(frame, 0, 0);
+    mvwaddstr(frame, 0, (COLS - strlen(argv[1])) >> 1, argv[1]);
+    wrefresh(frame);
+
+    view = newwin(LINES - 2, COLS - 2, 1, 1);
+    keypad(view, TRUE);
+    scrollok(view, TRUE);
+
+    for (int ln = 0; ln < LINES - 3; ++ln) {
+        if (wprintw_f(view, file) == ERR) {
+            break;
+        }
+    }
+
+    for (int ch = wgetch(view); ch != 0x1b; ch = wgetch(view)) {
+        switch (ch) {
+        case 0x20:
+            wprintw_f(view, file);
+            break;
+        }
+    }
+
+    delwin(view);
+    delwin(frame);
+    endwin();
+    fclose(file);
+    return 0;
+}
