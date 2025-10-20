65cbe5af6bb8b4981219d3605dc6079f87a756bb
Valery Zabrovsky <valthebrewer@yandex.ru>
2025-10-20 16:51:59 +0300

    05_Regexps: Implement pattern substitution

---
+++ 05_Regexps/esub.c
@@ -0,0 +1,126 @@
+#include <regex.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char *
+patternsubst(
+        const char *restrict pattern,
+        const char *restrict orig_line,
+        size_t nmatch,
+        const regmatch_t *restrict pmatch
+)
+{
+    char *res = NULL;
+    char *pmem;
+    regoff_t backref;
+    size_t sz = pmatch[0].rm_so;
+    size_t last_sz;
+    size_t pattern_len = strlen(pattern);
+    size_t last_idx = 0;
+
+    if ((pmem = malloc(sz)) == NULL) {
+        fprintf(stderr, "Memory error: Not enough space for malloc()\n");
+        return NULL;
+    } else {
+        res = pmem;
+    }
+
+    memcpy(res, orig_line, sz);
+
+    for (size_t idx = 0; idx < pattern_len; ++idx) {
+        if (pattern[idx] != '\\') {
+            continue;
+        }
+
+        last_sz = sz;
+        sz += idx - last_idx;
+
+        if ((pmem = realloc(res, sz)) == NULL) {
+            fprintf(stderr, "Memory error: Not enough space for realloc()\n");
+            free(res);
+            return NULL;
+        } else {
+            res = pmem;
+        }
+
+        memcpy(&res[last_sz], &pattern[last_idx], idx - last_idx);
+        last_idx = idx;
+        backref = pattern[++idx] - '0';
+
+        if (1 <= backref && backref <= 9) {
+            if (backref > (regoff_t) nmatch) {
+                fprintf(stderr,
+                        "Substitution error: Back reference \\%d is invalid\n",
+                        backref);
+                free(res);
+                return NULL;
+            }
+
+            last_sz = sz;
+            sz += pmatch[backref].rm_eo - pmatch[backref].rm_so;
+            res = realloc(res, sz);
+            memcpy(&res[last_sz], &orig_line[pmatch[backref].rm_so],
+                    sz - last_sz);
+            last_idx = idx + 1;
+        }
+    }
+
+    last_sz = sz;
+    sz += (pattern_len - last_idx) + (strlen(orig_line) - pmatch[0].rm_eo) + 1;
+
+    if ((pmem = realloc(res, sz)) == NULL) {
+        fprintf(stderr, "Memory error: Not enough space for realloc()\n");
+        free(res);
+        return NULL;
+    } else {
+        res = pmem;
+    }
+
+    memcpy(&res[last_sz], &pattern[last_idx], pattern_len - last_idx);
+    last_sz += pattern_len - last_idx;
+    memcpy(&res[last_sz], &orig_line[pmatch[0].rm_eo], sz - last_sz);
+    return res;
+}
+
+int
+main(int argc, char *argv[])
+{
+    regex_t regex;
+    regmatch_t *groups;
+    char *pstr;
+    size_t slen;
+    int rc;
+
+    if (argc < 4) {
+        fprintf(stderr, "usage: %s REGEX SUBST STR\n", argv[0]);
+        return 1;
+    }
+
+    if ((rc = regcomp(&regex, argv[1], REG_EXTENDED)) != 0) {
+        slen = regerror(rc, &regex, NULL, 0);
+        pstr = malloc(slen);
+        regerror(rc, &regex, pstr, slen);
+        fprintf(stderr, "Regex compilation error %d: %s\n", rc, pstr);
+        free(pstr);
+        return 2;
+    }
+
+    groups = calloc(regex.re_nsub + 1, sizeof(*groups));
+
+    if ((rc = regexec(&regex, argv[3], regex.re_nsub + 1, groups, 0)) != 0) {
+        printf("%s\n", argv[3]);
+        rc = 0;
+    } else if ((pstr = patternsubst(argv[2], argv[3], regex.re_nsub, groups))
+            != NULL) {
+        printf("%s\n", pstr);
+        free(pstr);
+        rc = 0;
+    } else {
+        rc = 2;
+    }
+
+    free(groups);
+    regfree(&regex);
+    return rc;
+}
