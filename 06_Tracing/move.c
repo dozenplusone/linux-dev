b9155e21142d2ede9bae1b4ec5d395e0831a8db5
Valery Zabrovsky <valthebrewer@yandex.ru>
2025-10-27 00:08:13 +0300

    06_Tracing: Perform same file check

--- 06_Tracing/move.c
+++ 06_Tracing/move.c
@@ -6,7 +6,8 @@
 int
 main(int argc, char *argv[])
 {
-    struct stat st;
+    struct stat st_in;
+    struct stat st_out;
     unsigned char *buf = NULL;
     int fd = -1;
 
@@ -14,14 +15,19 @@ main(int argc, char *argv[])
         return 1;
     }
 
+    stat(argv[1], &st_in);
+
+    if (stat(argv[2], &st_out) == 0 && st_out.st_ino == st_in.st_ino) {
+        return 2;
+    }
+
     fd = open(argv[1], O_RDONLY);
-    fstat(fd, &st);
-    buf = malloc(st.st_size);
-    read(fd, buf, st.st_size);
+    buf = malloc(st_in.st_size);
+    read(fd, buf, st_in.st_size);
     close(fd);
 
-    fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, st.st_mode);
-    write(fd, buf, st.st_size);
+    fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, st_in.st_mode);
+    write(fd, buf, st_in.st_size);
     free(buf);
     close(fd);
 
