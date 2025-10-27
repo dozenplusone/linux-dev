da6e9fdc7f05a263d2d9f35c77aa37ba9c895db8
Valery Zabrovsky <valthebrewer@yandex.ru>
2025-10-27 23:40:38 +0300

    06_Tracing: Make preloaded `unlink` reentrant

--- 06_Tracing/preload.c
+++ 06_Tracing/preload.c
@@ -2,9 +2,13 @@
 #include <errno.h>
 #include <string.h>
 
+static int (*true_unlink)(const char *) = NULL;
+
 int unlink(const char *name)
 {
-    int (*true_unlink)(const char *) = dlsym(RTLD_NEXT, "unlink");
+    if (true_unlink == NULL) {
+        true_unlink = dlsym(RTLD_NEXT, "unlink");
+    }
 
     if (strstr(name, "PROTECT") == NULL) {
         return true_unlink(name);
