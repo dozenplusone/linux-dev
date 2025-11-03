0711c8b3ea46328e3ba28ea061f44c3c4a4daca7
Valery Zabrovsky <valthebrewer@yandex.ru>
2025-11-03 16:23:11 +0300

    07_Environmental: Implement basic RHasher

---
+++ 07_Environmental/rhasher.c
@@ -0,0 +1,69 @@
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+
+#include "rhash.h"
+
+enum { MAX_DIGEST_SIZE = 64 };
+
+int
+main(void)
+{
+    unsigned char digest[MAX_DIGEST_SIZE];
+    char formatted_digest[2 + 2*MAX_DIGEST_SIZE];
+    char *line = NULL;
+    size_t linesz;
+    char *file_str;
+    unsigned algo;
+
+    rhash_library_init();
+
+    while (getline(&line, &linesz, stdin) != -1) {
+        if ((file_str = strchr(line, ' ')) == NULL) {
+            fprintf(stderr, "Invalid command format\n");
+            goto ERR;
+        }
+
+        *strchr(line, '\n') = '\0';
+        *file_str++ = '\0';
+
+        if (!strcasecmp(line, "md5")) {
+            algo = RHASH_MD5;
+        } else if (!strcasecmp(line, "sha1")) {
+            algo = RHASH_SHA1;
+        } else if (!strcasecmp(line, "tth")) {
+            algo = RHASH_TTH;
+        } else {
+            fprintf(stderr, "Unsupported hash algorithm\n");
+            goto ERR;
+        }
+
+        if (file_str[0] == '"') {
+            ++file_str;
+
+            if (rhash_msg(algo, file_str, strlen(file_str), digest) == -1) {
+                fprintf(stderr, "Failed to compute the digest\n");
+                goto ERR;
+            }
+        } else {
+            if (rhash_file(algo, file_str, digest) == -1) {
+                fprintf(stderr, "Failed to compute the digest for '%s': %s\n",
+                        file_str, strerror(errno));
+                goto ERR;
+            }
+        }
+
+        rhash_print_bytes(formatted_digest, digest,
+                rhash_get_digest_size(algo),
+                'a' <= line[0] && line[0] <= 'z' ? RHPR_BASE64 : RHPR_HEX);
+        printf("%s\n", formatted_digest);
+ERR:
+        free(line);
+        line = NULL;
+    }
+
+    free(line);
+    return 0;
+}
