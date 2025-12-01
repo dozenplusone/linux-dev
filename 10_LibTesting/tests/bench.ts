7946c46cb32e17c772d65a2528dfb627e4309b2d
Valery Zabrovsky <valthebrewer@yandex.ru>
2025-12-01 23:07:07 +0300

    10_LibTesting: Add checkmk-friendly tests

---
+++ 10_LibTesting/tests/bench.ts
@@ -0,0 +1,44 @@
+#include "buf.h"
+
+#include <stdint.h>
+
+#suite benchmark
+#tcase bench
+
+uint32_t
+pcg32(uint64_t *s)
+{
+    uint64_t m = 0x5851f42d4c957f2d;
+    uint64_t a = 0x8b260b70b8e98891;
+    uint64_t p = *s;
+    uint32_t x = ((p >> 18) ^ p) >> 27;
+    uint32_t r = p >> 59;
+    *s = p * m + a;
+    return (x >> r) | (x << (-r & 31u));
+}
+
+unsigned long
+bench(uint64_t *rng)
+{
+    unsigned long r = 0;
+    uint32_t n = 1000000 + pcg32(rng) % 4000000;
+    float *buf = 0;
+
+    for (uint32_t i = 0; i < n; i++)
+        buf_push(buf, pcg32(rng) / (double)UINT32_MAX);
+
+    float threshold = pcg32(rng) / (double)UINT32_MAX;
+    for (uint32_t i = 0; i < n; i++)
+        r += buf[i] > threshold;
+
+    buf_free(buf);
+    return r;
+}
+
+#test bench_
+    uint64_t rng = 0x558d2750ae0e0887;
+    unsigned long r = 0;
+    for (int i = 0; i < 300; i++) {
+        r += bench(&rng);
+    }
+    ck_assert_uint_eq(r, 428859598);
---
+++ 10_LibTesting/tests/unit.ts
@@ -0,0 +1,69 @@
+#include "buf.h"
+
+float *a = NULL;
+long *ai = NULL;
+
+#suite unittest
+
+#tcase basic
+
+#test init_push_clear_free
+    ck_assert_uint_eq(buf_capacity(a), 0);
+    ck_assert_uint_eq(buf_size(a), 0);
+    buf_push(a, 1.3f);
+    ck_assert_uint_eq(buf_size(a), 1);
+    ck_assert_float_eq(a[0], 1.3f);
+    buf_clear(a);
+    ck_assert_uint_eq(buf_size(a), 0);
+    ck_assert_ptr_nonnull(a);
+    buf_free(a);
+    ck_assert_ptr_null(a);
+
+#test clear_empty
+    buf_clear(a);
+    ck_assert_uint_eq(buf_size(a), 0);
+    ck_assert_ptr_null(a);
+
+#tcase ops
+
+void
+teardown(void)
+{
+    buf_free(a);
+    buf_free(ai);
+}
+
+#test push_
+    for (int i = 0; i < 10000; i++) {
+        buf_push(ai, i);
+    }
+    ck_assert_uint_eq(buf_size(ai), 10000);
+    size_t match = 0;
+    for (size_t i = 0; i < buf_size(ai); i++) {
+        match += ai[i] == i;
+    }
+    ck_assert_uint_eq(match, 10000);
+
+#test grow_trunc
+    buf_grow(ai, 1000);
+    ck_assert_uint_eq(buf_capacity(ai), 1000);
+    ck_assert_uint_eq(buf_size(ai), 0);
+    buf_trunc(ai, 100);
+    ck_assert_uint_eq(buf_capacity(ai), 100);
+
+#test pop_
+    buf_push(a, 1.1);
+    buf_push(a, 1.2);
+    buf_push(a, 1.3);
+    buf_push(a, 1.4);
+    ck_assert_uint_eq(buf_size(a), 4);
+    ck_assert_float_eq(buf_pop(a), 1.4f);
+    buf_trunc(a, 3);
+    ck_assert_uint_eq(buf_size(a), 3);
+    ck_assert_float_eq(buf_pop(a), 1.3f);
+    ck_assert_float_eq(buf_pop(a), 1.2f);
+    ck_assert_float_eq(buf_pop(a), 1.1f);
+    ck_assert_uint_eq(buf_size(a), 0);
+
+#main-pre
+    tcase_add_checked_fixture(tc1_2, NULL, teardown);
