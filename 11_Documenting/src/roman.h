05a9772fb4b30c0da88a289e4d7e6e49a620aba3
Valery Zabrovsky <valthebrewer@yandex.ru>
2025-12-07 11:57:02 +0300

    11_Documenting: Move Roman routines into a separate header

---
+++ 11_Documenting/src/roman.h
@@ -0,0 +1,118 @@
+#ifndef ROMAN_H
+#define ROMAN_H
+
+#include <stdlib.h>
+#include <string.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+static inline char *
+int2roman(int n)
+{
+    static const char letters[] = "IXCMVLD";
+    char *ans = NULL;
+    int len = 0;
+    int pos = sizeof(letters) - 1;
+    int den = 1000;
+
+    if (n < 1 || n > 3999) {
+        return NULL;
+    }
+
+    ans = calloc(sizeof("MMMDCCCLXXXVIII"), 1);
+
+    do {
+        int dig = n / den;
+
+        if (dig == 9) {
+            ans[len++] = letters[pos - 4];
+            ans[len++] = letters[pos - 3];
+        } else if (dig == 4) {
+            ans[len++] = letters[pos - 4];
+            ans[len++] = letters[pos];
+        } else {
+            if (dig >= 5) {
+                ans[len++] = letters[pos];
+                dig -= 5;
+            }
+
+            while (dig--) {
+                ans[len++] = letters[pos - 4];
+            }
+        }
+
+        --pos;
+        n %= den;
+        den /= 10;
+    } while (den);
+
+    return realloc(ans, len + 1);
+}
+
+static inline int
+roman2int(const char *str)
+{
+    static char letters[] = "MDCLXVI";
+    unsigned long pos = 0;
+    unsigned long len;
+    unsigned long count;
+    int factor = 100;
+    int ans = 0;
+    char tmp;
+
+    if (!str || !(len = strlen(str))) {
+        return 0;
+    }
+
+    if ((count = strspn(str, "M")) > 3) {
+        return -1;
+    }
+
+    ans += 1000 * count;
+    str += count;
+    len -= count;
+
+    do {
+        tmp = letters[pos + 3];
+        letters[pos + 3] = '\0';
+        count = strspn(str, letters + pos);
+
+        if (str[0] == letters[pos + 2] && str[1] == letters[pos]
+                && count == 2) {
+            ans += 9 * factor;
+        } else if (str[0] == letters[pos + 2] && str[1] == letters[pos + 1]
+                && count == 2) {
+            ans += 4 * factor;
+        } else if (count > 0) {
+            if (str[0] == letters[pos + 1]) {
+                ans += 5 * factor;
+                ++str;
+                --len;
+                --count;
+            }
+
+            if (strspn(str, letters + pos + 2) == count && count <= 3) {
+                ans += count * factor;
+            } else {
+                letters[pos + 3] = tmp;
+                return -1;
+            }
+        }
+
+        str += count;
+        len -= count;
+        letters[pos + 3] = tmp;
+        pos += 2;
+        factor /= 10;
+    } while (factor);
+
+    return len ? -1 : ans;
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* ROMAN_H */
