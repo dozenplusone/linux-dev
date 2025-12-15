1fbeee381ce8702e6882279def5f5c34c4279eaf
Valery Zabrovsky <valthebrewer@yandex.ru>
2025-12-15 22:23:45 +0300

    12_InstallPackaging: Enable Gettext and do i18n

--- 12_InstallPackaging/src/epoch2date.c
+++ 12_InstallPackaging/src/epoch2date.c
@@ -1,25 +1,27 @@
+#include "config.h"
 #include "utcdate.h"
 
 #include <errno.h>
+#include <libintl.h>
+#include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-static const char *const WEEKDAY_NAMES[] = {
-    "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat",
-};
-
-static const char *const MONTH_NAMES[] = {
-    "Jan", "Feb", "Mar", "Apr", "May", "Jun",
-    "Jul", "Aug", "Sep", "Oct", "Nov", "Dec",
-};
+#define _(STR) gettext(STR)
 
 int
 main(int argc, char *argv[])
 {
+    char *MONTH_NAMES[12];
+    char *WEEKDAY_NAMES[7];
     utc_datetime_t dt;
     int64_t stamp;
     weekday_t wd;
     int err;
+
+    setlocale(LC_ALL, "");
+    bindtextdomain(PACKAGE, LOCALE_PATH);
+    textdomain(PACKAGE);
 
     if (argc < 2) {
         return 1;
@@ -37,7 +39,28 @@ main(int argc, char *argv[])
         return 3;
     }
 
-    printf("%4$s %2$s %3$2hhu %5$02hhu:%6$02hhu:%7$02hhu UTC %1$04ld\n",
+    MONTH_NAMES[0] = _("Jan");
+    MONTH_NAMES[1] = _("Feb");
+    MONTH_NAMES[2] = _("Mar");
+    MONTH_NAMES[3] = _("Apr");
+    MONTH_NAMES[4] = _("May");
+    MONTH_NAMES[5] = _("Jun");
+    MONTH_NAMES[6] = _("Jul");
+    MONTH_NAMES[7] = _("Aug");
+    MONTH_NAMES[8] = _("Sep");
+    MONTH_NAMES[9] = _("Oct");
+    MONTH_NAMES[10] = _("Nov");
+    MONTH_NAMES[11] = _("Dec");
+
+    WEEKDAY_NAMES[0] = _("Sun");
+    WEEKDAY_NAMES[1] = _("Mon");
+    WEEKDAY_NAMES[2] = _("Tue");
+    WEEKDAY_NAMES[3] = _("Wed");
+    WEEKDAY_NAMES[4] = _("Thu");
+    WEEKDAY_NAMES[5] = _("Fri");
+    WEEKDAY_NAMES[6] = _("Sat");
+
+    printf(_("%4$s %2$s %3$2hhu %5$02hhu:%6$02hhu:%7$02hhu UTC %1$04ld\n"),
             dt.date.year, MONTH_NAMES[dt.date.month - 1], dt.date.day,
             WEEKDAY_NAMES[wd],
             dt.time.hours, dt.time.minutes, dt.time.seconds);
