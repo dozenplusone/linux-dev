89c9de6f52f43450439bdd468ac70b866ed5b837
Valery Zabrovsky <valthebrewer@yandex.ru>
2025-12-16 02:09:13 +0300

    12_InstallPackaging: Add Javadoc-styled comments

--- 12_InstallPackaging/include/utcdate.h
+++ 12_InstallPackaging/include/utcdate.h
@@ -39,15 +39,63 @@ typedef struct utc_datetime_s
     utc_time_t time;
 } utc_datetime_t;
 
+/** Check if a datetime at `pdt` is valid.
+ *
+ * @param[in] pdt pointer to the datetime being checked
+ *
+ * @result
+ * On success, 0 is returned.
+ * Otherwise, -1 is returned, and `errno` is set to indicate the error.
+ *
+ * @section Errors
+ * @b EFAULT @param pdt is invalid
+ */
 int
 dtcheck(utc_datetime_t *pdt);
 
+/** Get day of the week by date.
+ *
+ * @param[in] pd pointer to the date
+ *
+ * @result
+ * On success, the day of the week is returned.
+ * Otherwise, -1 is returned, and `errno` is set to indicate the error.
+ *
+ * @section Errors
+ * @b EFAULT @param pdt is invalid
+ */
 weekday_t
 get_weekday(utc_date_t *pd);
 
+/** Convert Unix timestamp to datetime.
+ *
+ * @param[in] stamp pointer to the date
+ * @param[out] pdt pointer to the datetime
+ *
+ * @result
+ * On success, 0 is returned.
+ * Otherwise, -1 is returned, and `errno` is set to indicate the error.
+ *
+ * @section Errors
+ * @b EFAULT @param pdt is invalid
+ */
 int
 timestamp2dt(int64_t stamp, utc_datetime_t *pdt);
 
+/** Convert datetime to Unix timestamp.
+ *
+ * @param[out] pstamp pointer to the stamp; may be NULL
+ * just to check if conversion is possible
+ * @param[in] pdt pointer to the datetime
+ *
+ * @result
+ * On success, 0 is returned.
+ * Otherwise, -1 is returned, and `errno` is set to indicate the error.
+ *
+ * @section Errors
+ * @b EFAULT @param pdt is invalid
+ * @b EOVERFLOW the datetime's timestamp cannot fit in 64 bits
+ */
 int
 dt2timestamp(int64_t *pstamp, utc_datetime_t *pdt);
 
